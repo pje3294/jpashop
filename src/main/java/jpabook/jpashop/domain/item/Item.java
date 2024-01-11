000ddd5d93e0c7866b514d0afd1b5d8fd2784880
parkje <jeongeun969@gmail.com>
2024-01-11 16:45:59 +0900

    상품 재고 증가/감소 로직 추가

--- src/main/java/jpabook/jpashop/domain/item/Item.java
+++ src/main/java/jpabook/jpashop/domain/item/Item.java
@@ -2,6 +2,7 @@ package jpabook.jpashop.domain.item;
 
 import jakarta.persistence.*;
 import jpabook.jpashop.domain.Category;
+import jpabook.jpashop.exception.NotEnoughStockException;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -27,4 +28,25 @@ public abstract class Item {
     @ManyToMany(mappedBy = "items")
     private List<Category> categories =  new ArrayList<>();
 
+    //-- 비스니스 로직 start -- //
+    /**
+     * stock 증가
+     */
+    public void addStock(int quantity) {
+        this.stockQuantity += quantity;
+    }
+
+    /**
+     * stock 감소
+     */
+    public void removeStock(int quantity) {
+        int restStock = this.stockQuantity - quantity;
+        if(restStock < 0) {
+            // exception 생성
+            throw new NotEnoughStockException("need more stock");
+        }
+        this.stockQuantity = restStock;
+    }
+
+    //-- 비스니스 로직 end -- //
 }
